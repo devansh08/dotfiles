d4c803ff72370cf5d4ef6caf3cc522cf39462f8f
Devansh Sharma <devansh.sharma@gmail.com>
2022-09-15 00:58:55 +0530

    Add lsblk alias

---
+++ lsblko.fish
@@ -0,0 +1,3 @@
+function lsblko --wraps='lsblk -o NAME,UUID,FSTYPE,FSUSE%,LABEL' --description 'alias lsblko=lsblk -o NAME,UUID,FSTYPE,FSUSE%,LABEL'
+  lsblk -o NAME,UUID,FSTYPE,FSUSE%,LABEL $argv; 
+end
