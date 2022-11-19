62df7e5361f062eea84683682e67616dfb84b632
Devansh Sharma <devansh.sharma@gmail.com>
2022-11-19 14:59:00 +0530

    Add code tree list function

---
+++ .config/fish/functions/ct.fish
@@ -0,0 +1,3 @@
+function ct --wraps='tree -a -I "node_modules" -I ".git" -I ".idea" -I ".vscode" .' --description 'alias ct tree -a -I "node_modules" -I ".git" -I ".idea" -I ".vscode" .'
+  tree -a -I "node_modules" -I ".git" -I ".idea" -I ".vscode" . $argv; 
+end
