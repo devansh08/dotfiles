cabf7d82d3b25d20aafc7bb9882f79131709bb6b
Devansh Sharma <devansh.sharma@gmail.com>
2023-05-18 09:55:59 +0530

    Add git diff with fzf alias

---
+++ .config/fish/functions/gdsf.fish
@@ -0,0 +1,3 @@
+function gdsf --wraps='git diff --staged --name-only --relative | fzf --preview="git diff --staged --color=always {1}" --layout=reverse' --description 'alias gdsf git diff --staged --name-only | fzf --preview="git diff --staged --color=always {1}" --layout=reverse'
+  git diff --staged --name-only --relative | fzf --preview="git diff --staged --color=always {1}" --layout=reverse $argv
+end
