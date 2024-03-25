dce26a573b71bb6da36e63021bba974f31569069
Devansh Sharma <devansh.sharma@gmail.com>
2024-03-25 18:30:06 +0530

    chore(fish): Change zoxide alias to `cdz`

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,5 +21,5 @@ if status --is-interactive
 
     fzf_configure_bindings --directory=\cf --git_log=\cl --git_status=\cs --history=\cr --variables=\cx --processes=\cp
     _cd_listener
-    zoxide init --cmd 'cd' fish | source
+    zoxide init --cmd cdz fish | source
 end
