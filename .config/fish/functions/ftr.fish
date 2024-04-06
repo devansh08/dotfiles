47d1bb97afb8e94f7817771ca4ff69b72f3377d6
Devansh Sharma <devansh.sharma@gmail.com>
2024-04-06 21:56:38 +0530

    fix(fish): Fix pwd in tmux resurrect fix

--- .config/fish/functions/ftr.fish
+++ .config/fish/functions/ftr.fish
@@ -1,3 +1,3 @@
-function ftr --wraps=rm\ \~/.local/share/tmux/resurrect/last\ \&\&\ ln\ -s\ \(/bin/ls\ -t1\ tmux_resurrect_\*\ \|\ head\ -n\ 2\ \|\ tail\ -n\ 1\)\ last\ \&\&\ find\ .\ -type\ f\ -name\ \'tmux_resurrect\*\'\ -empty\ -delete --description alias\ ftr\ rm\ \~/.local/share/tmux/resurrect/last\ \&\&\ ln\ -s\ \(/bin/ls\ -t1\ tmux_resurrect_\*\ \|\ head\ -n\ 2\ \|\ tail\ -n\ 1\)\ last\ \&\&\ find\ .\ -type\ f\ -name\ \'tmux_resurrect\*\'\ -empty\ -delete
-  rm ~/.local/share/tmux/resurrect/last && ln -s (/bin/ls -t1 tmux_resurrect_* | head -n 2 | tail -n 1) last && find . -type f -name 'tmux_resurrect*' -empty -delete $argv
+function ftr
+    set -l pwd (pwd) && cd ~/.local/share/tmux/resurrect && rm -f last && ln -s (/bin/ls -t1 tmux_resurrect_* | head -n 2 | tail -n 1) last && find . -type f -name 'tmux_resurrect*' -empty -delete $argv && cd $pwd
 end
