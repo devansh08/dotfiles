1b068b97509aedb80ac039666449756b79c3223e
Devansh Sharma <devansh.sharma@gmail.com>
2024-06-23 19:03:29 +0530

    feat(fish): Set Wayland Display variable with available socket

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -20,7 +20,7 @@ if status --is-interactive
     set -gx QT_QPA_PLATFORMTHEME qt5ct
     set -gx nvm_default_version lts
     set -gx XCURSOR_THEME Bibata-Modern-Classic
-    set -gx WAYLAND_DISPLAY wayland-0
+    set -gx WAYLAND_DISPLAY (basename (find /run/user/(id -u) -type s -name 'wayland-*' 2> /dev/null))
 
     fzf_configure_bindings --directory=\cf --git_log=\cl --git_status=\cs --history=\cr --variables=\cx --processes=\cp
     _cd_listener
