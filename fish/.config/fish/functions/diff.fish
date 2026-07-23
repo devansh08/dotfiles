9a7f4a3f5c33e80b741892c74a95d7aea56ed347
Devansh Sharma <devansh.sharma@gmail.com>
2026-07-24 00:08:01 +0530

    chore(fish): Add fish functions

---
+++ fish/.config/fish/functions/diff.fish
@@ -0,0 +1,3 @@
+function diff --wraps='git diff --no-index' --description 'alias diff git diff --no-index'
+    git diff --no-index $argv
+end
---
+++ fish/.config/fish/functions/pi.fish
@@ -0,0 +1,3 @@
+function pi --description 'alias pi pi --tools read,edit,write,bash,grep,find,ls,web_fetch'
+    command pi --tools read,edit,write,bash,grep,find,ls,web_fetch $argv
+end
